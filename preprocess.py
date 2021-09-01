3fdb497cab139667d41dd2e3d3696fcedd2589d2
Sina Mahdipour Saravani <sinamps@BusProdNAS01.busdom.colostate.edu>
2021-09-01 11:06:40 -0600

    Initial commit

---
+++ preprocess.py
@@ -0,0 +1,76 @@
+import os
+import email
+import re
+from email import policy, parser
+from email_reply_parser import EmailReplyParser
+import talon
+from talon import signature
+import pandas as p
+
+DIR = 'datasets/IWSPA 2.0 Train/'
+# FH_DIR = 'datasets/IWSPA 2.0 Train/IWSPA2.0_Training_Full_Header'
+# NH_DIR = 'datasets/IWSPA 2.0 Train/IWSPA2.0_Training_No_Header'
+
+
+def clean_remove_header(f):
+    email_msg = email.message_from_file(f, policy=policy.default)
+    # email_msg = parser.Parser(policy=policy.default).parse(fp=f)
+    # print(email_msg.get_body())
+    # print(email_msg['From'])
+    if email_msg.is_multipart():
+        # I need to fix this later so that we do not lose these three phishing samples
+        # print(f.name)
+        return '---sinamps08/31/2021---'
+    else:
+        payload = email_msg.get_payload()
+    # payload = email_msg.get_payload()
+    text = talon.quotations.extract_from_plain(payload)
+    # text = EmailReplyParser.parse_reply(payload)
+    text, sign = signature.bruteforce.extract_signature(text)
+    text, sign = signature.extract(text, sender='')
+    return text
+
+
+def get_data(dirname, remove_header, fc, isPhish):
+    texts_list = []
+    for filename in os.listdir(dirname):
+        with open(os.path.join(dirname, filename), 'r') as f:  # open in readonly mode
+            fc = fc + 1
+            if remove_header:
+                text = clean_remove_header(f)
+                if text == '---sinamps08/31/2021---':
+                    continue
+            else:
+                text = f.read()
+            texts_list.append(text)
+            f.close()
+    labels_list = [(1 if isPhish else 0) for i in range(len(texts_list))]
+    return texts_list, labels_list, fc
+
+
+def read_files(input_dir):
+    texts = []
+    labels = []
+    fc = 0
+    for dirname in os.listdir(input_dir):
+        remove_header = False
+        if dirname.endswith('Full_Header'):
+            remove_header = True
+        legit_dir = input_dir + dirname + '/legit/'
+        phish_dir = input_dir + dirname + '/phish/'
+        legit_texts, legit_labels, fc = get_data(legit_dir, remove_header, fc, isPhish=False)
+        phish_texts, phish_labels, fc = get_data(phish_dir,  remove_header, fc, isPhish=True)
+        texts.extend(legit_texts)
+        texts.extend(phish_texts)
+        labels.extend(legit_labels)
+        labels.extend(phish_labels)
+    dict = {'text': texts, 'label': labels}
+    df = p.DataFrame(data=dict)
+    # print(fc)
+    # print(df)
+    df.to_csv('phishing_dataset.csv')
+
+
+if __name__ == '__main__':
+    talon.init()
+    read_files(DIR)
